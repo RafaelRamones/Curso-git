340405059bd68e7da914baea9c9233b04d136e37
Rafael Henrique <rafaelramones21@hotmail.com>
2020-08-04 18:58:40 -0300

    Projeto iniciado, mostrando a mensagem Ola Mundo

---
+++ Teste git/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Teste_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+
+            Console.ReadLine();
+        }
+    }
+}
